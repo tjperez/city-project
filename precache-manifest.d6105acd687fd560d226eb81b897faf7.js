7813223874c2d435392c3bc0f9b448c07f6b39a2
tjlinares <tjlinares23@gmail.com>
2019-06-10 12:42:24 -0400

    Updates

---
+++ precache-manifest.d6105acd687fd560d226eb81b897faf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35699e4a546f57bcd57e4ad27402b1f1",
+    "url": "/city-project/index.html"
+  },
+  {
+    "revision": "40b8cdb14384a954efbf",
+    "url": "/city-project/static/css/main.ff75b6a3.chunk.css"
+  },
+  {
+    "revision": "73d4bd57d2948c9e60cd",
+    "url": "/city-project/static/js/2.b2ca986f.chunk.js"
+  },
+  {
+    "revision": "40b8cdb14384a954efbf",
+    "url": "/city-project/static/js/main.7d653963.chunk.js"
+  },
+  {
+    "revision": "8e7c4d2ab39e40ee2c7f",
+    "url": "/city-project/static/js/runtime~main.28a3c139.js"
+  }
+]);
